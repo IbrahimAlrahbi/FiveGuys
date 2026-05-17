38d94eb1e949fbb28dc58ede7bfc5c09a1371a03
ibrahbi <ibra7bi@gmail.com>
2026-05-17 17:02:30 +0400

    Team FiveGuys

---
+++ src/main/java/com/example/fiveguys/FiveguysApplication.java
@@ -0,0 +1,14 @@
+package com.example.fiveguys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiveguysApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome Team FiveGuys !");
+		SpringApplication.run(FiveguysApplication.class, args);
+	}
+
+}
