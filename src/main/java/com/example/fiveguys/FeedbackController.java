ec52eea9a587de0fc89bba0c6b4f657c3e15dc90
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-05-18 08:36:12 +0400

    Add in-memory feedback storage

--- src/main/java/com/example/fiveguys/FeedbackController.java
+++ src/main/java/com/example/fiveguys/FeedbackController.java
@@ -3,7 +3,12 @@ package com.example.fiveguys;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/feedback")
 public class FeedbackController {
+    private List<Feedback> feedbackList = new ArrayList<>();
+
 }
