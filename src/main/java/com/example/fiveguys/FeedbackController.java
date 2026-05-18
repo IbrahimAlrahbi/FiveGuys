5f8ee972b17bfad00996c180e8983c9ea5e299dc
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-05-18 08:38:03 +0400

    Add POST endpoint for feedback creation

--- src/main/java/com/example/fiveguys/FeedbackController.java
+++ src/main/java/com/example/fiveguys/FeedbackController.java
@@ -1,5 +1,8 @@
 package com.example.fiveguys;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,6 +16,11 @@ public class FeedbackController {
     private List<Feedback> feedbackList = new ArrayList<>();
 
     private int nextId = 1;
+
+    @PostMapping
+    public ResponseEntity<Feedback> createFeedback(@RequestBody FeedbackRequest request) {
+
+    }
 
 
 }
