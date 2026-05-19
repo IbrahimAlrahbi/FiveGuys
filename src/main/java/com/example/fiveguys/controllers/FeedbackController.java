458e97c01450e0bef0cdd2c61004b2367f9d1658
ibrahbi <ibra7bi@gmail.com>
2026-05-19 08:08:17 +0400

    Creating Post in Services

--- src/main/java/com/example/fiveguys/controllers/FeedbackController.java
+++ src/main/java/com/example/fiveguys/controllers/FeedbackController.java
@@ -2,6 +2,7 @@ package com.example.fiveguys.controllers;
 
 import com.example.fiveguys.models.Feedback;
 import com.example.fiveguys.services.FeedbackService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,20 +16,14 @@ import java.util.List;
 // Base URL for all feedback endpoints
 @RequestMapping("/feedback")
 public class FeedbackController {
-    // Stores feedback entries temporarily in memory
-    private List<Feedback> feedbackList = new ArrayList<>();
+    @Autowired
+    public FeedbackService feedbackService;
 
-    // Generates unique IDs for feedback entries
-    private int nextId = 1;
 
     // Handles POST requests to create new feedback
     @PostMapping
-    public ResponseEntity<Feedback> createFeedback(@RequestBody FeedbackService request) {
-        Feedback feedback = new Feedback(nextId, request.getContent());
-        feedbackList.add(feedback);
-        nextId++;
-        // Return HTTP 201 Created with feedback data
-        return ResponseEntity.status(201).body(feedback);
+    public ResponseEntity<Feedback> createFeedback(@RequestBody Feedback incomingFeedback) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(feedbackService.createFeedback(incomingFeedback));
     }
 
     @GetMapping
--- src/main/java/com/example/fiveguys/services/FeedbackService.java
+++ src/main/java/com/example/fiveguys/services/FeedbackService.java
@@ -1,14 +1,30 @@
 package com.example.fiveguys.services;
 
 import com.example.fiveguys.models.Feedback;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
+@Service
 public class FeedbackService {
     // Stores feedback text from client request
-    private String content;
+    private final List<Feedback> feedbackList = new ArrayList<>();
 
     // Returns feedback content
-    public String getContent() {
-        return content;
+    // Generates unique IDs for feedback entries
+    private int nextId = 1;
+
+    public List<Feedback> getAllFeedbacks() {
+        return feedbackList;
+    }
+
+    public Feedback getFeedback(int id) {
+        for (Feedback feedback : feedbackList) {
+            if (feedback.getId() == id) {
+                return feedback;
+            }
+        }
+        return null;
     }
 
     // Updates feedback content
